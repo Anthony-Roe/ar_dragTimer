40d346a163dbf5884ac7b3e6c9f482faa597436d
Anthony Roe <antroe36@gmail.com>
2020-03-05 19:34:16 -0500

    push files

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+ui_page "client/nui/index.html"
+
+files {
+    "client/nui/index.html",
+    "client/nui/index.js",
+    "client/nui/style.css",
+    "client/nui/bootstrap.css",
+}
+
+client_scripts {
+    "client/cl_dragTimer.net.dll",
+}
